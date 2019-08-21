7c8a80874e4206015f90d0a17670adb1507b770c
Megan Wilson <mlw7v3@mail.missouri.edu>
2019-08-20 23:45:24 -0500

    added file to repo

---
+++ Expenses/Expenses/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Expenses
+//
+//  Created by Megan Wilson on 8/20/19.
+//  Copyright © 2019 Megan Wilson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
